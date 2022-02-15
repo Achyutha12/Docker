5ca742c397834f211cf6146de13805e68293f113
Achyutha12 <96127510+Achyutha12@users.noreply.github.com>
2022-02-15 13:17:05 +0530

    Add files via upload

---
+++ CRUD/CrudAPI/Controllers/StudentController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CrudAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : Controller
+    {
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+
+        public static ArrayList StudentData = new ArrayList() { "Scott","Smith"};
+
+        
+        [HttpGet]
+        [Route("GetAllStudents")]
+        public ArrayList GetAllUsers()
+        {
+            return StudentData;
+        }
+
+        //[HttpGet]
+        //[Route("GetStudent")]
+        //public string GetStudent(int id)
+        //{
+        //    return StudentData[id].ToString();
+        //}
+
+        [HttpPost]
+        [Route("SaveNewStudent")]
+        public ArrayList SaveNewStudent([FromQuery] string student)
+        {
+            StudentData.Add(student);
+            return StudentData;
+        }
+
+        [HttpPut]
+        [Route("UpdateStudent")]
+        public ArrayList UpdateStudent(int id, [FromQuery] string student)
+        {
+            StudentData[id] = student;
+            return StudentData;
+        }
+
+        [HttpDelete]
+        [Route("DeleteStudent")]
+
+        public ArrayList DeleteStudent(int id, [FromQuery] string student)
+        {
+            StudentData[id] = student;
+            StudentData.Remove(student);
+            return StudentData;
+        }
+    }
+}
